b9774627d8a1f006f952c82d9405f8ed6bb8d3a8
Paul Stetsenko <28633195+p-stets@users.noreply.github.com>
2021-02-26 18:09:13 +0200

    function instead of method

--- mysite/helpers.py
+++ mysite/helpers.py
@@ -1,20 +1,13 @@
 from mysite.settings import OPERATOR_CODES
 
 
-class Cell(object):
-
+def codes_regexp(codes_list=OPERATOR_CODES, additional_digits_count=7):
     '''
-    Helping cell methods to use
+    Takes a list of mobile codes as "codes_list" argument
+    Takes number of numbers that can go after code as "additional_digits_count"
+    Returns regex to catch cell number.
     '''
-
-    @staticmethod
-    def codes_regexp(codes_list=OPERATOR_CODES, additional_digits_count=7):
-        '''
-        Takes a list of mobile codes as "codes_list" argument
-        Takes number of numbers that can go after code as "additional_digits_count"
-        Returns regex to catch cell number.
-        '''
-        for el in list(enumerate(codes_list[:-1])):
-            codes_list[el[0]] = el[1] + '|'
-        regex_list = ''.join(x for x in codes_list)
-        return f'^0({regex_list})\\d{{{additional_digits_count}}}/$'
+    for el in list(enumerate(codes_list[:-1])):
+        codes_list[el[0]] = el[1] + '|'
+    regex_list = ''.join(x for x in codes_list)
+    return f'^0({regex_list})\\d{{{additional_digits_count}}}/$'
