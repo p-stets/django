9d84e4654f6622c42b254d0163dcadcb92c9cdfa
Paul Stetsenko <28633195+p-stets@users.noreply.github.com>
2021-03-09 19:01:09 +0200

    update

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import Article, BlogUser, Comment, Like
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date', 'updated_date', )
+
+
+class BlogUserAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date', 'updated_date', )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date', 'updated_date', )
+
+
+class LikeAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date', 'updated_date', )
+
+
 # Register your models here.
-admin.site.register(BlogUser)
-admin.site.register(Article)
-admin.site.register(Comment)
-admin.site.register(Like)
+admin.site.register(BlogUser, BlogUserAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Like, LikeAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.db.models.base import Model
@@ -7,6 +8,10 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
+# a = Comment.objects.all().order_by('-id').values('content')[:5]
+# >>> Comment.objects.create(content='Finish', related_article_id=1, author_id=1)
+
+
 class BlogUser(models.Model):
     '''
     A blog user
@@ -14,6 +19,8 @@ class BlogUser(models.Model):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Last update date')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -28,9 +35,11 @@ class Article(models.Model):
     content = models.TextField()
     author = models.ForeignKey(
         'BlogUser', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Article Author')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Last update date')
 
     def __str__(self):
-        return self.title
+        return f'{self.author} - {self.id}'
 
 
 class Comment(models.Model):
@@ -45,9 +54,25 @@ class Comment(models.Model):
     related_comment = models.ForeignKey(
         'self', verbose_name='Related Comment', on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField(verbose_name='Content')
-
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Last update date')
     # A validation to make sure either related_article or related_comment is populated; not both
     # and that the author is also present
+
+    def save(self):
+        year_before = datetime(
+            year=datetime.now().year - 1,
+            month=datetime.now().month,
+            day=datetime.now().day,
+            hour=datetime.now().hour,
+            minute=datetime.now().minute,
+            second=datetime.now().second
+        )
+        self.updated_date = year_before
+        print(self.updated_date)
+        yeear = self.objects.all().filter(id=self.id)
+        print(yeear)
+
     def clean(self):
         if (self.related_article and self.related_comment) or (not self.related_article and not self.related_comment):
             connection_error_message = 'The comment should be connected either to article or to comment'
@@ -81,6 +106,8 @@ class Like(models.Model):
         ContentType, on_delete=models.CASCADE, related_name='activity_logs')
     object_id = models.BigIntegerField(default=None, null=True)
     object = GenericForeignKey(ct_field="content_type", fk_field="object_id")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Last update date')
 
 
 # Base comment way
